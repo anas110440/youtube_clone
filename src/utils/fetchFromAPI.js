2a869d01502574319bff86f0cfd4cf25478697d2
anas110440 <75642207+anas110440@users.noreply.github.com>
2023-02-12 04:47:50 +0300

    last update

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.CI,
+    'X-RapidAPI-Key': '69ee00773emsh9bb47da3af35c07p16dba8jsnff6976f03f19',
 
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
